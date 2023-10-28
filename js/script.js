f36232516a808fffb3031cd4da22b446875a8716
Łukasz Piechota <piechotapiano@gmail.com>
2023-10-28 23:37:11 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
+let resultElement = document.querySelector(".js-result");
+
+let rateEUR = 4;
+let rateUSD = 5;
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+
+    let result;
+
+    switch (currency) {
+
+        case "EUR":
+            result = amount * rateEUR;
+            break;
+
+        case "USD":
+            result = amount * rateUSD;
+            break;
+    }
+
+    resultElement.innerHTML = `Za ${amount.toFixed(2)} PLN otrzymasz <strong>${result.toFixed(2)} ${currency}
+ </strong > `;
+    
+
+});
